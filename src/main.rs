38feaeaf47dfd8de7f86e2a130d783ad5a8282b1
Noah Joeris <noahjoeris@gmail.com>
2026-02-19 15:06:14 +0200

    add axum webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("listening on http://localhost:3000");
+    axum::serve(listener, app).await.unwrap();
 }
